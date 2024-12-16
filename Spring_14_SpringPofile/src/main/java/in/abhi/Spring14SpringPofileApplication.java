a89a39b7ba8b967f288aa648ca1894bd4acbd144
ABHIMANYU KUMAR <155754944+abhimanyu07Kr@users.noreply.github.com>
2024-12-16 22:56:10 +0530

    Add files via upload

---
+++ Spring_14_SpringPofile/src/main/java/in/abhi/Spring14SpringPofileApplication.java
@@ -0,0 +1,13 @@
+package in.abhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring14SpringPofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring14SpringPofileApplication.class, args);
+	}
+
+}
